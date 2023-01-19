d1dcd9cfb1042ddfe7172f2bdf217e9cf8749144
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-01-19 10:36:20 +0100

    Production build

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Navbar } from './Navbar';
 export { default as Hero } from './Hero';
-export { default as CountUp } from './CountUp';
-export { default as Newsletter } from './Newsletter';
 export { default as Why } from './Why';
+export { default as Calculate } from './Calculate';
+export { default as Market } from './Market';
+export { default as Newsletter } from './Newsletter';
 export { default as Footer } from './Footer';
