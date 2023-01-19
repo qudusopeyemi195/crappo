c18c71720446648afc42664de923180d8543ea7a
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-01-19 10:33:45 +0100

    Production build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import Logo from '../assets/images/logo.png';
 
 const Button = ({ children, classProps }) => {
   return (
-    <button className={`flex ${classProps} px-3 py-2 items-center active:-translate-y-1 cursor-pointer hover:-translate-y-2 duration-300 text-white font-[600] bg-[#3b82f6] rounded-2xl rounded-tl-none rounded-br-none`}>
+    <button className={`flex ${classProps} px-3 py-2 items-center cursor-pointer active:-translate-y-1 hover:-translate-y-2 duration-300 text-white font-[600] bg-[#3b82f6] rounded-2xl rounded-tl-none rounded-br-none`}>
       {children}
     </button>
   );
@@ -44,13 +44,13 @@ const Navbar = () => {
   window.addEventListener('scroll', handleNavBg);
 
   return (
-    <nav className={`fixed top-0 z-[100] w-full h-20 md:px-10 px-4 ${navBg 
+    <nav className={`fixed top-0 z-100 w-full h-16 md:h-20 ${navBg 
       ? 'shadow-xl z-[100] bg-[#0D0D2B] ease-in-out duration-300' 
       : ''}`} 
-     > {/**Maybe to remove top-0 */}
-      <div className="flex justify-between items-center w-full h-full max-w-[1200px] m-auto"> {/**max-w-[1200px] */}
-        <Link to="hero" smooth={true} offset={50} duration={500}>
-          <div className="flex justify-beween items-center py-2 space-x-4 cursor-pointer">
+     >
+      <div className="flex justify-between items-center w-full h-full md:px-10 px-4 max-w-[1200px] mx-auto"> 
+        <Link to="hero" smooth={true} offset={2} duration={500}>
+          <div className="flex items-center md:space-x-4 space-x-2 cursor-pointer"> 
             <img src={Logo} alt="/" width={40} />
             <h1 className="text-lg">CRAPPO</h1>
           </div>
@@ -66,46 +66,44 @@ const Navbar = () => {
               <Button>Register</Button>
             </li>
           </ul>
-          <div onClick={() => setToggleMenu(!toggleMenu)} className="md:hidden cursor-pointer z-[10]">
+          <div onClick={() => setToggleMenu(!toggleMenu)} className="md:hidden cursor-pointer z-10">
             <AiOutlineMenu size={25} color={'white'} />
           </div>
         </div>
       </div>
-      {/* Mobile Menu */}
-      {/* Overlay */}
-      <div className={toggleMenu ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-lack/70' : ''}>
-        {/* Side Drawwer Menu */}
+      {/* Mobile Nav */}
+      <div className={toggleMenu ? 'md:hidden fixed left-0 top-0 w-full h-full bg-black/70' : ''}> 
         <div className={toggleMenu
-          ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#0D0D2B] p-4 ease-in duration-500' 
-          : 'fixed left-[-100%] top-0 p-4 ease-in duration-500'}
+          ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-full  bg-[#0D0D2B] p-4 ease-in duration-500' 
+          : 'fixed left-[-100%] top-0 ease-in duration-500'}
         >
           <div>
             <div className="flex justify-between items-center w-full">
-              <Link to="hero" smooth={true} offset={50} duration={500} onClick={() => setToggleMenu(false)}>
-                <div className="flex justify-between items-center space-x-4 py-2 cursor-pointer">
+              <Link to="hero" smooth={true} offset={2} duration={500} onClick={() => setToggleMenu(false)}>
+                <div className="flex items-center space-x-2 cursor-pointer">
                   <img src={Logo} alt="" width={40} />
                   <h1 className="text-lg">CRAPPO</h1>
                 </div>
               </Link>
-              <div className="p-3 rounded-full shadow-sm cursor-pointer" onClick={() => setToggleMenu(false)}>
+              <div className="p-3 rounded-full shadow-sm shadow-black cursor-pointer" onClick={() => setToggleMenu(false)}>
                 <AiOutlineClose />
               </div>
             </div>
-            <div className="border-b border-gray-300 my-2">
+            <div className="border-b border-gray-300 my-3">
               <p className="w-[85%] md:w-[90%] py-3">Let's trade crypto together</p>
             </div>
           </div>
-          <div className="flex flex-col py-2">
+          <div className="flex flex-col space-y-2">
             <ul className="text-white list-none">
-              {['Products', 'Features', 'About', 'Contact'].map((item, index) => (
+              {['Products', 'Features', 'About', 'Contact', 'Login', 'Register'].map((item, index) => (
                 <NavbarItem 
                   key={item + index} 
                   title={item} 
                   handleClick={() => setToggleMenu(false)}
-                  classProps="my-6" />
+                  classProps="my-4" />
               ))}
             </ul>
-            <div className="pt-4">
+            <div>
               <p className="uppercase tracking-widest text-[#3671E9]">Connect With Us</p>
               <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
                 {[<FaLinkedinIn />, <FaGithub />, <AiOutlineMail />, <FaFacebook />].map((link, index) => (
@@ -120,7 +118,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
