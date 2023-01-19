32fa3ee7f85dc46fe1a6f68eccd562c529ddb3ed
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-01-19 10:31:42 +0100

    Production build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { Footer, Hero, Navbar, Why } from "./components";
+import { Calculate, Footer, Hero, Market, Navbar, Newsletter, Why } from "./components";
 
 function App() {
   return (
-    <div className="text-[#FFFFFF] tracking-wide h-screen">
+    <div className="text-[#FFFFFF] tracking-wide w-full h-full overflow-hidden">
       <Navbar />
       <Hero />
       <Why />
+      <Calculate />
+      <Market />
+      <Newsletter />
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
