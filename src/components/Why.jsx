52af58771824e1f6d0892b61fcb63121a35dc6f3
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2023-01-19 10:32:13 +0100

    Production buile

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -5,9 +5,9 @@ import crappo from "../assets/images/whycrappo.png";
 
 const Why = () => {
   return (
-    <div className="relative w-full max-w-[1200px] m-auto md:px-10 px-4 lg:pt-32 py-16">
+    <section className="relative w-full max-w-[1200px] mx-auto md:px-10 px-4 lg:pt-32 pt-0 pb-4 md:16"> {/**bg-white/5  */}
       <CountUp />
-      <div className="flex justify-between items-center space-x-8 w-full pt-16">
+      <div className="grid md:grid-cols-2 gap-8 justify-between items-center w-full pt-4 md:pt-16">
         <div className="relative">
           <Rotate duration={3000}>
             <img src={crappo} alt="/" />
@@ -16,13 +16,13 @@ const Why = () => {
         </div>
         <Fade duration={3000}>
           <div>
-            <h2 className="text-4xl md:text-5xl font-bold">Why you should choose CRAPPO</h2>
+            <h2 className="text-2xl sm:text-4xl md:text-5xl font-bold">Why you should choose CRAPPO</h2>
             <p className="pt-4 text-md md:w-96">Experience the next generation cryptocurrency platform. No financial borders, extra fees, and fake reviews.</p>
-            <button className="bg-[#3671E9] rounded-full px-5 py-3 mt-4 shadow-lg cursor-pointer outline-0 hover:-translate-y-2 active:-translate-y-1 duration-300">Learn More</button>
+            <button className="bg-[#3671E9] rounded-full px-5 py-3 mt-5 shadow-lg cursor-pointer outline-0 hover:-translate-y-2 active:-translate-y-1 duration-300">Learn More</button>
           </div>
         </Fade>
       </div>
-    </div>
+    </section>
   );
 };
 
